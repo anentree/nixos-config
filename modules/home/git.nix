162638b35cb50e52c4814360d9939d2f954699bc
anentree <brantleygunn@gmail.com>
2024-10-03 11:14:53 -0500

    initial changes test

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -3,8 +3,8 @@
   programs.git = {
     enable = true;
     
-    userName = "Frost-Phoenix";
-    userEmail = "67cyril6767@gmail.com";
+    userName = "anentree";
+    userEmail = "brantleygunn@gmail.com";
     
     extraConfig = { 
       init.defaultBranch = "main";
